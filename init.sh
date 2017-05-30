569872c4285bca9719c2e4c15472e5710b0502f7
Arseny <arseny.moskvichev@gmail.com>
2017-05-30 14:22:28 +0300

    New init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
+
+
