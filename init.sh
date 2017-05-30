2627a738f49452a9b1a98d2e008c84bd4aa8ee1f
Arseny <arseny.moskvichev@gmail.com>
2017-05-30 15:02:04 +0300

    Modify init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn -c /etc/gunicorn.d/hello.py hello
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello
 
 
