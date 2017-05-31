f5d387c48c9ca0875322faedc410820161cbea8f
Arseny <arseny.moskvichev@gmail.com>
2017-05-31 15:11:21 +0300

    Change allowed hosts typo

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kt0uo+ium_op5s^pzey&f$#%79_l-%)z!vsmyc9e40=-4=3yar'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.qa',
+    'app.app',
 ]
 
 MIDDLEWARE = [
