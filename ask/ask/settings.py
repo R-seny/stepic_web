17bb30a496a46d6ed9fce12f8f097352dc260026
Arseny <arseny.moskvichev@gmail.com>
2017-05-31 15:15:40 +0300

    new settings

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.qa',
-    'app.app',
+    'qa',
+    'ask',
 ]
 
 MIDDLEWARE = [
