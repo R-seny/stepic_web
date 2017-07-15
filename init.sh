a8256460617cc4b9ccde67f8c34ec09be472a9ae
Arseny <arseny.moskvichev@gmail.com>
2017-07-15 13:55:49 +0300

    one more change to init

--- init.sh
+++ init.sh
@@ -6,12 +6,12 @@ sudo /etc/init.d/nginx restart
 
 mysql -uroot -e "create database qadb"
 
-cd ~home/box/web/ask
+cd /home/box/web/ask
 
 python manage.py makemigrations qa
 python manage.py migrate
 
-cd ~home/box/web
+cd /home/box/web
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 #sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application &
 sudo gunicorn -c ./etc/qa_conf_stepic.py  ask.wsgi:application &
